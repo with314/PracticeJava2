6c73f94c2fe88b17ca7fd18a6f3cd1ca1c966e66
An <81519430+with314@users.noreply.github.com>
2023-04-14 21:22:21 +0900

    Add files via upload

---
+++ OkJavaGoInHome.java
@@ -0,0 +1,29 @@
+package Programming;
+
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OKJavaGoInHome {
+   public static void main(String[] args) {
+
+            // Elevator call
+            Elevator myElevator = new Elevator("JAVA APT 507");
+            myElevator.callForUp(1);
+
+            // Security off
+            Security mySecurity = new Security("JAVA APT 507");
+            mySecurity.off();
+
+            // light on
+            Lighting hallLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+            hallLamp.on();
+
+            Lighting floorLamp = new Lighting("JAVA APT 507 / floorLamp");
+            floorLamp.on();
+
+
+        }
+    }
+
+}
